50270a5608a9d231b41813f086e31be0638f3877
KaungMyatNaing <kotoyarsan@gmail.com>
2023-11-03 19:08:37 +0630

    just test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    int catNumber = 100;
+    //I gonna cast some number.
+    int dumbNumber = (int) 10L;
+    System.out.println(dumbNumber);
+    System.out.println("Cat naumber ->"+catNumber);
+    System.out.println("This is the beginning.");
+  }
+}
